3cad1d9ad8d365723f92137e896c522c99a9a78d
Selina Wu <selinawu@Selinas-MacBook-Air.local>
2025-10-17 22:18:27 -0700

    Added move empty position functions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 class EightPuzzle:
 
-    def __init__(self, puzz_board, move, depth, cost, parent, initial_state = None):
+    def __init__(self, move, depth, cost, parent, initial_state = None):
 
         # Initialize start state
         if initial_state is None:
@@ -12,7 +12,6 @@ class EightPuzzle:
 
         self.empty_pos = self.find_empty_pos()
 
-        self.puzz_board = puzz_board #current board state
         self.move = move #movement of blank tile (up, down, left, right)
         self.depth = depth #depth of search tree (same as cost for UCS)
         self.cost = cost #depth + heuristic
@@ -30,6 +29,10 @@ class EightPuzzle:
     def is_goal(self):
 
         return None
+    
+    # Override < so that priority queue knows lowest cost is being prioritized
+    def __lt__(self, other):
+        return self.cost < other.cost
 
 class EightPuzzleSolver:
 
@@ -47,6 +50,58 @@ class EightPuzzleSolver:
     def a_star_search_manhattan(puzzle, heuristic):
         
         return None
+    
+    # Create new board state after moving empty tile left
+    @staticmethod
+    def move_left(state, empty_pos):
+        new_state = [x[:] for x in state] #make copy of current board state
+        i, j = empty_pos
+
+        if j > 0: #make sure empty tile isn't at leftmost column
+            # Swap empty tile w/ tile on its left
+            new_state[i][j], new_state[i][j - 1] = new_state[i][j - 1], new_state[i][j]
+            return new_state
+        else:
+            return None
+        
+    # Create new board state after moving empty tile right
+    @staticmethod
+    def move_right(state, empty_pos):
+        new_state = [x[:] for x in state] #make copy of current board state
+        i, j = empty_pos
+
+        if j < 2: #make sure empty tile isn't at rightmost column
+            # Swap empty tile w/ tile on its right
+            new_state[i][j], new_state[i][j + 1] = new_state[i][j + 1], new_state[i][j]
+            return new_state
+        else:
+            return None
+
+    # Create new board state after moving empty tile up
+    @staticmethod
+    def move_up(state, empty_pos):
+        new_state = [x[:] for x in state] #make copy of current board state
+        i, j = empty_pos
+
+        if i > 0: #make sure empty tile isn't at topmost row
+            # Swap empty tile w/ tile above
+            new_state[i][j], new_state[i - 1][j] = new_state[i - 1][j], new_state[i][j]
+            return new_state
+        else:
+            return None
+        
+    # Create new board state after moving empty tile down
+    @staticmethod
+    def move_down(state, empty_pos):
+        new_state = [x[:] for x in state] #make copy of current board state
+        i, j = empty_pos
+
+        if i < 2: #make sure empty tile isn't at bottom-most row
+            # Swap empty tile w/ tile below
+            new_state[i][j], new_state[i + 1][j] = new_state[i + 1][j], new_state[i][j]
+            return new_state
+        else:
+            return None
 
 if __name__ == "__main__":
 
